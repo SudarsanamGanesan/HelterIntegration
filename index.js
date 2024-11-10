d390f9487883fd593a9f7ee82c2f81d5c6888cea
Sudarsanam <sudarsanam@barberaa.com>
2024-11-10 14:24:49 +0530

    server is working

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const axios = require("axios");
+require('dotenv').config();
+const cors=require('cors')
+
+
+
+const app = express();
+const port = 3001;
+
+app.use(cors())
+
+// Access variables from .env file
+const sendBulkCampaignAPI = process.env.SEND_BULK_CAMPAIGN_API;
+const secretKey = process.env.SECRET_KEY;
+
+app.use(express.json());
+
+app.post("/send-campaign", async (req, res) => {
+    try {
+        let myNumber = req.body.phoneNo;
+
+        const apiBody = {
+            campaignName: "campaignName",
+            campaignDesc: "campaignDesc",
+            templateName: "barberaa_test",
+            languageCode: "en",
+            templateContent: "templateContent",
+            messages: [{
+                clientWaNumber: myNumber.startsWith("91") ? myNumber : "91" + myNumber,
+                variables: []
+            }]
+        };
+
+        const response = await axios.post(sendBulkCampaignAPI, apiBody, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${secretKey}`
+            }
+        });
+
+        res.status(200).json({
+            message: "Campaign sent successfully!",
+            data: response.data
+        });
+
+    } catch (error) {
+        console.error("Error sending campaign:", error);
+        res.status(500).json({ error: "Failed to send campaign" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
